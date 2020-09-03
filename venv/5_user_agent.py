38bfa8b523313aaf787011774952a569876fd8db
Ljw <ljws93@naver.com>
2020-09-03 23:33:46 +0900

    py_webScrap_bs4_find_200903
    user-agent
    bs4 - soup
    bs4 - find

---
+++ venv/5_user_agent.py
@@ -0,0 +1,10 @@
+import requests
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"}
+url = "https://nadocoding.tistory.com/"
+res = requests.get(url, headers = headers)
+
+res.raise_for_status()
+
+
+with open("tistory.html", "w", encoding="utf8") as f:
+    f.write(res.text)
---
+++ venv/6_bs4.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/weekday.nhn"
+
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+
+webtoon = soup.find("a", text="외모지상주의")
+
+print(webtoon)
